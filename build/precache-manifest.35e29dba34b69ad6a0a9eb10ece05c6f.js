7988ea69e52377760f27366a245d6168520f5f4a
Anawin <anawin.chu@gmail.com>
2021-03-23 15:02:41 +0700

    3rd commit

---
+++ build/precache-manifest.35e29dba34b69ad6a0a9eb10ece05c6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2701f37957efbfe63b27119bdb9df5b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2a20011a5ca77ef60cd",
+    "url": "/static/css/main.aa21a8a9.chunk.css"
+  },
+  {
+    "revision": "8d4b670ae6775a50635a",
+    "url": "/static/js/2.2a163f1a.chunk.js"
+  },
+  {
+    "revision": "d2a20011a5ca77ef60cd",
+    "url": "/static/js/main.ea1f418b.chunk.js"
+  },
+  {
+    "revision": "b4b68f9c55d5b736a6e1",
+    "url": "/static/js/runtime-main.f930ed43.js"
+  }
+]);
